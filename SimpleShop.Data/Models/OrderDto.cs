b16cbbf9323cbcd35d2ea6cf6b3380caed856e38
ProgWidness <ProgWidness@users.noreply.github.com>
2021-03-19 16:12:51 +0300

    Create dtos

---
+++ SimpleShop.Data/Models/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleShop.Data.Models
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public ClientDto Client { get; set; }
+        public Dictionary<ProductDto, int> Products { get; set; }
+    }
+}
---
+++ SimpleShop.Data/Models/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleShop.Data.Models
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public CategoryDto Category { get; set; }
+        public BrandDto Brand { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+        public decimal Length { get; set; }
+        public decimal Weight { get; set; }
+        public decimal PowerConsumption { get; set; }
+    }
+}
